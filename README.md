17734278fb70f350e5738a86598471820c98067a
eugene <eugenekobiliatsky@gmail.com>
2026-08-03 11:14:44 +0300

    Handled 2 diodes blinking delay with 2 buttons - increase and decrease. Added wokwi simulation, updated readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # emb16-homework
-Main branch for embedded development homework.
-Check feature branches for actual code.
+Control of 2 diodes blink with 2 buttons:
+ - external button increases delay;
+ - internal Boot button decreases;
+ - Default and new values printed on console during change;
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,122 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+constexpr uint8_t RED_LED_PIN = 15;
+constexpr uint8_t BLUE_LED_PIN = 16;
+
+constexpr uint8_t EXTERN_BUTTON_PIN = 21;
+constexpr uint8_t BOOT_BUTTON_PIN = 0;
+
+constexpr uint16_t MIN_BLINK_DELAY = 50;
+constexpr uint16_t MAX_BLINK_DELAY = 1000;
+constexpr uint16_t BLINK_STEP = 100;
+
+constexpr uint16_t DEBOUNCE_TIME = 50;
+
+uint16_t blinkDelay = 200;
+
+uint8_t externalLastReading = HIGH;
+uint8_t externalStableState = HIGH;
+unsigned long externalLastDebounceTime = 0;
+
+uint8_t bootLastReading = HIGH;
+uint8_t bootStableState = HIGH;
+unsigned long bootLastDebounceTime = 0;
+
+bool ledState = LOW;
+
+uint8_t currentBlink = 0;
+
+unsigned long lastBlinkTime = 0;
+
+void initHardware();
+void handleButtons();
+bool wasButtonPressed(uint8_t pin, uint8_t &lastReading, uint8_t &stableState, unsigned long &lastDebounceTime);
+void handleBlinking();
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  initHardware();
+  Serial.printf("Blink delay: %u ms\n", blinkDelay);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  handleButtons();
+  handleBlinking();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void initHardware() {
+  pinMode(RED_LED_PIN, OUTPUT);
+  pinMode(BLUE_LED_PIN, OUTPUT);
+
+  digitalWrite(RED_LED_PIN, LOW);
+  digitalWrite(BLUE_LED_PIN, LOW);
+  pinMode(EXTERN_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(BOOT_BUTTON_PIN, INPUT_PULLUP);
+}
+
+void handleButtons() {
+  if (
+    wasButtonPressed(
+      EXTERN_BUTTON_PIN,
+      externalLastReading,
+      externalStableState,
+      externalLastDebounceTime
+    )
+  ) {
+    if (blinkDelay + BLINK_STEP <= MAX_BLINK_DELAY)
+      blinkDelay += BLINK_STEP;
+
+    Serial.printf("External button: blink delay = %u ms\n", blinkDelay);
+  }
+
+  if (
+    wasButtonPressed(
+      BOOT_BUTTON_PIN,
+      bootLastReading,
+      bootStableState,
+      bootLastDebounceTime
+    )
+  ) {
+    if (blinkDelay >= MIN_BLINK_DELAY + BLINK_STEP)
+      blinkDelay -= BLINK_STEP;
+
+    Serial.printf("BOOT button: blink delay = %u ms\n", blinkDelay);
+  }
+}
+
+bool wasButtonPressed(
+    uint8_t pin,
+    uint8_t &lastReading,
+    uint8_t &stableState,
+    unsigned long &lastDebounceTime
+) {
+  uint8_t currentReading = digitalRead(pin);
+
+  if (currentReading != lastReading) {
+    lastDebounceTime = millis();
+    lastReading = currentReading;
+  }
+
+  if (millis() - lastDebounceTime >= DEBOUNCE_TIME) {
+    if (currentReading != stableState) {
+      stableState = currentReading;
+
+      if (stableState == LOW) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+void handleBlinking() {
+  if (millis() - lastBlinkTime >= blinkDelay) {
+    ledState = !ledState;
+
+    digitalWrite(RED_LED_PIN, ledState);
+    digitalWrite(BLUE_LED_PIN, ledState);
+
+    lastBlinkTime = millis();
+  }
 }
