021c6140aa6ec65f1399683805b41f54c9e9a8d4
eugene <eugenekobiliatsky@gmail.com>
2026-08-05 14:18:48 +0300

    Added code that reads, interperts and combine results into readable .md table. Added readme description

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,32 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+constexpr uint8_t ADC_RESOLUTION = 12;
+constexpr uint16_t ADC_MAX = (1 << ADC_RESOLUTION) - 1;
+constexpr uint8_t VOLTAGE_PIN = 4;
+constexpr uint16_t READING_DELAY = 3000;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(9600);
+    delay(1000);
+
+    analogReadResolution(ADC_RESOLUTION);
+    analogSetPinAttenuation(VOLTAGE_PIN, ADC_11db);
+
+	Serial.println("| Raw Data | U calc (mV) | U read (mV) | Error (%) |");
+	Serial.println("|  :---:   |    :---:    |    :---:    |   :---:   |");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+    uint16_t rawValue = analogRead(VOLTAGE_PIN);
+    uint32_t millivolts = analogReadMilliVolts(VOLTAGE_PIN);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    float uCalc = (static_cast<float>(rawValue) / ADC_MAX) * 3300.0f;
+    float measurError = ((uCalc - millivolts) / millivolts) * 100;
+
+    Serial.printf(
+        "|   %4d   | %11.2f | %10lu  | %+9.2f |\n",
+        rawValue, uCalc, millivolts, measurError
+    );
+
+    delay(READING_DELAY);
 }
