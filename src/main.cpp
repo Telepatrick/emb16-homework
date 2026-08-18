b566543b8ab956b30f649db419cb2c7a901c9a36
eugene <eugenekobiliatsky@gmail.com>
2026-08-18 11:35:39 +0300

    Implemented control over 3 diodes in loop without blocking. Updated readme with porcess description

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,15 @@ constexpr uint8_t LED_BLUE_PIN = 17;
 
 constexpr unsigned long LED_RED_INTERVAL = 200;
 constexpr unsigned long LED_WHITE_INTERVAL = 500;
-constexpr unsigned long LED_WHITE_INTERVAL = 1000;
+constexpr unsigned long LED_BLUE_INTERVAL = 1000;
 
 unsigned long ledRedPreviousMillis = 0;
 unsigned long ledWhitePreviousMillis = 0;
 unsigned long ledBluePreviousMillis = 0;
 
-bool led1State = LOW;
-bool led2State = LOW;
-bool led3State = LOW;
+bool ledRedState = LOW;
+bool ledWhiteState = LOW;
+bool ledBlueState = LOW;
 
 void setup()
 {
@@ -22,9 +22,9 @@ void setup()
     pinMode(LED_WHITE_PIN, OUTPUT);
     pinMode(LED_BLUE_PIN, OUTPUT);
 
-    digitalWrite(LED_RED_PIN, led1State);
-    digitalWrite(LED_WHITE_PIN, led2State);
-    digitalWrite(LED_BLUE_PIN, led3State);
+    digitalWrite(LED_RED_PIN, ledRedState);
+    digitalWrite(LED_WHITE_PIN, ledWhiteState);
+    digitalWrite(LED_BLUE_PIN, ledBlueState);
 }
 
 void loop()
@@ -35,23 +35,23 @@ void loop()
     {
         ledRedPreviousMillis = currentMillis;
 
-        led1State = !led1State;
-        digitalWrite(LED_RED_PIN, led1State);
+        ledRedState = !ledRedState;
+        digitalWrite(LED_RED_PIN, ledRedState);
     }
 
     if (currentMillis - ledWhitePreviousMillis >= LED_WHITE_INTERVAL)
     {
         ledWhitePreviousMillis = currentMillis;
 
-        led2State = !led2State;
-        digitalWrite(LED_WHITE_PIN, led2State);
+        ledWhiteState = !ledWhiteState;
+        digitalWrite(LED_WHITE_PIN, ledWhiteState);
     }
 
     if (currentMillis - ledBluePreviousMillis >= LED_BLUE_PIN)
     {
         ledBluePreviousMillis = currentMillis;
 
-        led3State = !led3State;
-        digitalWrite(LED_BLUE_PIN, led3State);
+        ledBlueState = !ledBlueState;
+        digitalWrite(LED_BLUE_PIN, ledBlueState);
     }
 }
