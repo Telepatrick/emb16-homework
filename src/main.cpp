946b2d4de6f0d1388f180075ca85a2051381b145
eugene <eugenekobiliatsky@gmail.com>
2026-08-18 09:36:19 +0300

    Implemented 3 diodes independent blinking

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,57 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+constexpr uint8_t LED_RED_PIN = 15;
+constexpr uint8_t LED_WHITE_PIN = 16;
+constexpr uint8_t LED_BLUE_PIN = 17;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+constexpr unsigned long LED_RED_INTERVAL = 200;
+constexpr unsigned long LED_WHITE_INTERVAL = 500;
+constexpr unsigned long LED_WHITE_INTERVAL = 1000;
+
+unsigned long ledRedPreviousMillis = 0;
+unsigned long ledWhitePreviousMillis = 0;
+unsigned long ledBluePreviousMillis = 0;
+
+bool led1State = LOW;
+bool led2State = LOW;
+bool led3State = LOW;
+
+void setup()
+{
+    pinMode(LED_RED_PIN, OUTPUT);
+    pinMode(LED_WHITE_PIN, OUTPUT);
+    pinMode(LED_BLUE_PIN, OUTPUT);
+
+    digitalWrite(LED_RED_PIN, led1State);
+    digitalWrite(LED_WHITE_PIN, led2State);
+    digitalWrite(LED_BLUE_PIN, led3State);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+    unsigned long currentMillis = millis();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    if (currentMillis - ledRedPreviousMillis >= LED_RED_INTERVAL)
+    {
+        ledRedPreviousMillis = currentMillis;
+
+        led1State = !led1State;
+        digitalWrite(LED_RED_PIN, led1State);
+    }
+
+    if (currentMillis - ledWhitePreviousMillis >= LED_WHITE_INTERVAL)
+    {
+        ledWhitePreviousMillis = currentMillis;
+
+        led2State = !led2State;
+        digitalWrite(LED_WHITE_PIN, led2State);
+    }
+
+    if (currentMillis - ledBluePreviousMillis >= LED_BLUE_PIN)
+    {
+        ledBluePreviousMillis = currentMillis;
+
+        led3State = !led3State;
+        digitalWrite(LED_BLUE_PIN, led3State);
+    }
 }
