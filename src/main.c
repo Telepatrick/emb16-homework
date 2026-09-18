b48506d95538c9926122e8e41e496237bf38af5d
eugene <eugenekobiliatsky@gmail.com>
2026-09-18 11:24:19 +0300

    Implemented handling of LED over LDR-based light-sensor, applied SMA-filter. Added readme description and gif demo.

--- src/main.c
+++ src/main.c
@@ -1,57 +1,98 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "driver/gpio.h"
 #include "esp_adc/adc_oneshot.h"
-#include "esp_adc/adc_cali.h"
-#include "esp_adc/adc_cali_scheme.h"
+#include "esp_log.h"
 
-#define ADC_UNIT ADC_UNIT_1
-#define ADC_CHAN ADC_CHANNEL_3
+#define LED_GPIO GPIO_NUM_5
+#define ADC_CHANNEL ADC_CHANNEL_3
 #define ADC_ATTEN ADC_ATTEN_DB_12
-#define ADC_BITWIDTH ADC_BITWIDTH_12
 
-#define V_REF 3300.0f
-#define ADC_MAX_VAL 4095.0f
+#define SMA_WINDOW_SIZE 10
 
-static adc_oneshot_unit_handle_t adc_handle;
-static adc_cali_handle_t cali_handle;
+#define DARK_THRESHOLD 120
+#define LIGHT_THRESHOLD 220
 
-static void init_hw(void)
+typedef struct {
+    int buffer[SMA_WINDOW_SIZE];
+    int index;
+    int count;
+    long sum;
+} sma_filter_t;
+
+void sma_init(sma_filter_t *filter)
 {
-    adc_oneshot_unit_init_cfg_t init_cfg = { .unit_id = ADC_UNIT };
-    adc_oneshot_new_unit(&init_cfg, &adc_handle);
+    for (int i = 0; i < SMA_WINDOW_SIZE; i++) {
+        filter->buffer[i] = 0;
+    }
 
-    adc_oneshot_chan_cfg_t chan_cfg = { .bitwidth = ADC_BITWIDTH, .atten = ADC_ATTEN };
-    adc_oneshot_config_channel(adc_handle, ADC_CHAN, &chan_cfg);
+    filter->index = 0;
+    filter->count = 0;
+    filter->sum = 0;
+}
 
-    adc_cali_curve_fitting_config_t cali_cfg = {
-        .unit_id = ADC_UNIT,
-        .chan = ADC_CHAN,
-        .atten = ADC_ATTEN,
-        .bitwidth = ADC_BITWIDTH,
+int sma_update(sma_filter_t *filter, int new_val)
+{
+    filter->sum -= filter->buffer[filter->index];
+    filter->buffer[filter->index] = new_val;
+    filter->sum += new_val;
+
+    filter->index = (filter->index + 1) % SMA_WINDOW_SIZE;
+
+    if (filter->count < SMA_WINDOW_SIZE) {
+        filter->count++;
+    }
+
+    return (int)(filter->sum / filter->count);
+}
+
+static void init_hw(adc_oneshot_unit_handle_t *adc_handle)
+{
+    gpio_reset_pin(LED_GPIO);
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
+    gpio_set_level(LED_GPIO, 0);
+
+    adc_oneshot_unit_init_cfg_t init_config1 = {
+        .unit_id = ADC_UNIT_1,
+        .clk_src = ADC_RTC_CLK_SRC_DEFAULT,
     };
-    adc_cali_create_scheme_curve_fitting(&cali_cfg, &cali_handle);
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, adc_handle));
+
+    adc_oneshot_chan_cfg_t config = {
+        .bitwidth = ADC_BITWIDTH_DEFAULT, // 12-bit (0 - 4095)
+        .atten = ADC_ATTEN,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(*adc_handle, ADC_CHANNEL, &config));
 }
 
 void app_main(void)
 {
-    init_hw();
+    adc_oneshot_unit_handle_t adc1_handle;
 
-    printf("| Raw Data | U calc (mV) | U read (mV) | Error (%%) |\n");
-    printf("|  :---:   |    :---:    |    :---:    |   :---:   |\n");
+    init_hw(&adc1_handle);
+
+    sma_filter_t ldr_filter;
+    sma_init(&ldr_filter);
+
+    bool led_state = false;
 
     while (1) {
-        int raw_val = 0;
-        int calib_mv = 0;
+        int raw_adc = 0;
+        ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, ADC_CHANNEL, &raw_adc));
 
-        adc_oneshot_read(adc_handle, ADC_CHAN, &raw_val);
-        adc_cali_raw_to_voltage(cali_handle, raw_val, &calib_mv);
+        int filtered_adc = sma_update(&ldr_filter, raw_adc);
 
-        float u_calc = ((float)raw_val / ADC_MAX_VAL) * V_REF;
-        float error = (calib_mv > 0) ? ((u_calc - calib_mv) / calib_mv) * 100.0f : 0.0f;
-
-        printf("|   %4d   | %11.2f | %10d  | %+9.2f |\n", raw_val, u_calc, calib_mv, error);
+        if (!led_state && filtered_adc < DARK_THRESHOLD) {
+            led_state = true;
+            gpio_set_level(LED_GPIO, 1);
+        } else if (led_state && filtered_adc > LIGHT_THRESHOLD) {
+            led_state = false;
+            gpio_set_level(LED_GPIO, 0);
+        }
 
         vTaskDelay(pdMS_TO_TICKS(100));
     }
+
+    adc_oneshot_del_unit(adc1_handle);
 }
